6ede3e0463b31e84e8a31f1d5664daa11979727c
Pham Minh <phamvanminh1723@gmail.com>
2024-03-25 17:24:39 +0700

    Them 3 file moi

---
+++ p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Multiples of 3 or 5
+
+int main()
+{
+    int sum = 0;
+    for (int i = 3; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
+    std::cout << sum;
+}
---
+++ p2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define LIMIT 4e6
+
+// Even Fibonacci Numbers
+
+int main()
+{
+    int f0 = 0;
+    int f1 = 1;
+    int fn = 1;
+    int sum = 0;
+    while (fn < LIMIT)
+    {
+        sum += fn % 2 ? 0 : fn;
+        f0 = f1;
+        f1 = fn;
+        fn = f0 + f1;
+    }
+    std::cout << sum;
+}
